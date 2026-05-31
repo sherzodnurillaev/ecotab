674ea122c290181075fd496452b9fc7eeace79f1
Amal <sherzod1700@mail.ru>
2026-05-31 23:08:26 +0500

    add suspans

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import CategoryPages from "@/components/mainPage/categoryPages";
-import { getCategories } from "@/lib/categories";
-import { getProducts } from "@/lib/products";
+import { Suspense } from "react"
+import CategoryPages from "@/components/mainPage/categoryPages"
+import { getCategories } from "@/lib/categories"
+import { getProducts } from "@/lib/products"
 
 export default async function Home() {
   const products = await getProducts()
   const categories = await getCategories()
 
   return (
-    <div className="">
-      <main className="pb-[20px]">
+    <main className="pb-[20px]">
+      <Suspense fallback={<div>Loading...</div>}>
         <CategoryPages products={products} categories={categories} />
-      </main>
-    </div>
-  );
+      </Suspense>
+    </main>
+  )
 }
